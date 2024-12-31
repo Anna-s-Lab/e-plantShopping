70abef744968ab2146fbb2ac12343ccc785bc2dc
Ana De Souza <ana.desouza@segu360.com>
2024-12-31 15:29:28 -0300

    Defining Cart Slice reducers

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,19 +1,29 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const CartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState: {
     items: [], // Initialize items as an empty array
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const { name, image, cost } = action.payload;
+      // Check if item already exists in the cart
+      const item = state.items.find((item) => item.name === name);
+      if (item) item.quantity++;
+      else state.items.push({ name, image, cost, quantity: 1 });
     },
     removeItem: (state, action) => {
+      const { name } = action.payload;
+      const newList = state.items.filter((item) => item.name !== name);
+      state.items = newList;
     },
     updateQuantity: (state, action) => {
-
-    
+      const { name, amount } = action.payload;
+      const item = state.items.find((item) => item.name === name);
+      if (item) {
+        item.quantity = amount;
+      }
     },
   },
 });
